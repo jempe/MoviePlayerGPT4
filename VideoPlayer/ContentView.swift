adbb60665ecf1d4b6fad6b71361a4bf008e8f818
Jempe <info@jempe.org>
2023-03-14 18:21:45 -0500

    step 2 GPT-4

--- VideoPlayer/ContentView.swift
+++ VideoPlayer/ContentView.swift
@@ -1,9 +1,55 @@
 import SwiftUI
 import AVKit
+import AVFoundation
+
+struct VideoPlayerView: NSViewRepresentable {
+    let player: AVPlayer
+    
+    func makeNSView(context: Context) -> NSView {
+        let playerView = AVPlayerView()
+        playerView.player = player
+        return playerView
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {}
+}
+
+class AVPlayerView: NSView {
+    var player: AVPlayer? {
+        didSet {
+            (self.layer as? AVPlayerLayer)?.player = player
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        self.wantsLayer = true
+        self.layer = AVPlayerLayer()
+        (self.layer as? AVPlayerLayer)?.videoGravity = .resizeAspect
+    }
+}
 
 struct ContentView: View {
+    @StateObject private var player = AVPlayer(url: Bundle.main.url(forResource: "example", withExtension: "mp4")!)
+
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "example", withExtension: "mp4")!))
-            .frame(width: 400, height: 300)
+        VideoPlayerView(player: player)
+            .onAppear {
+                player.play()
+            }
+            .onDisappear {
+                player.pause()
+            }
+            .frame(minWidth: 640, minHeight: 480)
+            .edgesIgnoringSafeArea(.all)
     }
 }
