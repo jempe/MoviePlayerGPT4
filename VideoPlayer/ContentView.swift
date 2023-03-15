42e759abb3628fdc9d8ea6e72de269405bc33057
Jempe <info@jempe.org>
2023-03-14 19:39:16 -0500

    step 2 add File Panel

--- VideoPlayer/ContentView.swift
+++ VideoPlayer/ContentView.swift
@@ -2,8 +2,28 @@ import SwiftUI
 import AVKit
 
 struct ContentView: View {
+    @State private var videoURL: URL?
+    
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "example", withExtension: "mp4")!))
-            .frame(width: 400, height: 300)
+        VStack {
+            if let videoURL = videoURL {
+                VideoPlayer(player: AVPlayer(url: videoURL))
+                    .frame(width: 400, height: 300)
+            } else {
+                Text("Please select a video file")
+            }
+            
+            Button("Select Video File") {
+                let openPanel = NSOpenPanel()
+                openPanel.allowedFileTypes = ["mp4"] // add more file types as needed
+                openPanel.canChooseFiles = true
+                openPanel.canChooseDirectories = false
+                openPanel.allowsMultipleSelection = false
+                
+                if openPanel.runModal() == .OK {
+                    videoURL = openPanel.url
+                }
+            }
+        }
     }
 }
